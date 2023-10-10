89d4386ffccc0ed15702e1db1227797263a1c618
Liam <lharvey7@cps.edu>
2023-10-10 10:37:47 -0500

    first country

---
+++ .github/Countries.py
@@ -0,0 +1,14 @@
+country_db: List[Tuple[str, str, int, List[str]] ] = [
+    (
+        "United States", # Name
+        "None",     # Official language
+        1776,       # Year founded
+        [
+            "Joe Biden",        # Most recent Leaders
+            "Donald Trump",
+            "Barack Obama",
+            "George W. Bush",
+            "Bill clinton"
+        ]
+    )
+]
