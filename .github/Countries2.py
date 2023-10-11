121452fda5aa66796260f9eb74d139c3534ea4d6
Maged <mmothana@cps.edu>
2023-10-11 10:58:17 -0500

    8 added countries

---
+++ .github/Countries2.py
@@ -0,0 +1,96 @@
+    (
+        "United Kingdom",
+        "English",
+        1707,
+        [
+            "Rishi Sunak",
+            "Liz Truss",
+            "Boris Johnson",
+            "Theresa May",
+            "David Cameron"
+        ]
+    ),
+    (
+        "France",
+        "French",
+        1792,
+        [
+            "Emmanual Macron",
+            "François Hollande",
+            "Nicolas Sarkozy",
+            "Jacques Chirac",
+            "François Mitterrand"
+        ]
+    ),
+    (
+        "Germany",
+        "German",
+        1871,
+        [
+            "Frank-Walter Steinmeier",
+            "Joachim Gauck",
+            "Christian Wulff",
+            "Horst Köhler",
+            "Johannes Rau"
+        ]
+    )
+    (
+        "Italy",
+        "Italian",
+        1861,
+        [
+            "Giorgia Meloni",
+            "Mario Draghi",
+            "Giuseppe Conte",
+            "Paolo Gentiloni",
+            "Matteo Renzi"
+        ]
+    )
+    (
+        "Spain",
+        "Spanish",
+        1978,
+        [
+            "Pedro Sánchez",
+            "Mariano Rajoy",
+            "José Luis Rodríguez Zapatero",
+            "José María Aznar",
+            "Felipe González"
+        ]
+    )
+    (
+        "Portugal",
+        "Portuguese",
+        1143,
+        [
+            "Marcelo Rebelo de Sousa",
+            "Aníbal Cavaco Silva",
+            "Jorge Sampaio",
+            "Mário Soares",
+            "António Ramalho Eanes"
+        ]
+    )
+    (
+        "Türkiye",
+        "Turkish",
+        1923,
+        [
+            "Recep Tayyip Erdoğan",
+            "Abdullah Gül",
+            "Ahmet Necdet Sezer",
+            "Süleyman Demirel",
+            "Turgut Özal"
+        ]
+    )
+    (
+        "Australia",
+        "None",
+        1901,
+        [
+            "Anthony Albanese",
+            "Scott Morrison",
+            "Malcolm Turnbull",
+            "Tony Abbott",
+            "Julia Gillard"
+        ]
+    )
