4fb48ebd338a24ae60773e20a027fea588dcc31a
Liam <lharvey7@cps.edu>
2023-10-11 10:58:53 -0500

    almost done with data base

--- .github/Countries.py
+++ .github/Countries.py
@@ -10,5 +10,91 @@ country_db: List[Tuple[str, str, int, List[str]] ] = [
             "George W. Bush",
             "Bill clinton"
         ]
+    ),
+    (
+        "China",
+        "Chinese",
+        1949,
+        [
+            "Xi Jinping",
+            "Hu Jintao",
+            "Jiang Zemin",
+            "Yang Shangkun",
+            "Li Xiannian"
+        ]
+    ),
+    (
+        "Japan",
+        "Japanese",
+        -660,
+        [
+            "Fumio Kishida",
+            "Yoshihide Suga",
+            "Shinzo Abe",
+            "Yoshihiko Noda",
+            "Naoto Kan"
+        ]
+                
+    ),
+    (
+        "South Korea",
+        "Korean",
+        1948,
+        [
+            "Yoon Suk-Yeol",
+            "Moon Jae-In",
+            "Park Geun-Hye",
+            "Lee Myung-Bak",
+            "Roh Moo-Hyun"
+        ]
+    ),
+    (
+        "Canada",
+        "English, French",
+        1867,
+        [
+            "Justin Trudeau",
+            "Stephen Harper",
+            "Paul Martin",
+            "Jean Chretien",
+            "Kim Campbell"
+        ]
+    ),
+    (
+        "Mexico",
+        "Spanish",
+        1821
+        [
+            "Andrés Manuel López Obrador",
+            "Enrique Peña Nieto",
+            "Felipe Calderón"
+            "Vicente Fox"
+            "Ernesto Zedillo"
+        ]
+    ),
+    (
+        "Singapore",
+        "Malay, English, Tamil, Singaporean Mandarin",
+        1965,
+        [
+            "Halimah Yacob",
+            "Tony Tan Keng Yam",
+            "S. R. Nathan",
+            "Ong Teng Cheong",
+            "Wee Kim Wee"
+        ]
+    ),
+    (
+        "Ukraine",
+        "Ukrainian",
+        1991,
+        [
+            "Volodymyr Zelenskyy",
+            "Petro Poroshenko",
+            "Oleksandr Turchynov",
+            "Viktor Yanukovych",
+            "Viktor Yushchenko"
+        ]
     )
+
 ]
