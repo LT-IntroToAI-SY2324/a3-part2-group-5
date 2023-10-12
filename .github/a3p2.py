c98ade6a07565b67e835f5fa86c455761fe7a031
Liam <lharvey7@cps.edu>
2023-10-12 10:31:09 -0500

    year_by_name

--- .github/a3p2.py
+++ .github/a3p2.py
@@ -7,3 +7,8 @@ def get_name(country: Tuple[str, str, int, List[str]]) -> str:
     return country[0]
 def get_language(country: Tuple[str, str, int, List[str]]) -> str:
     return country[1]
+def year_by_name(matches: List[str]) -> int:
+    for country in country_db:
+        if country[0] == matches:
+            return country[2]
+    return None
