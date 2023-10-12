b84a407be39932e4d021d8814cb5312c2e9bdcac
Liam <lharvey7@cps.edu>
2023-10-12 10:23:43 -0500

    get_name and get_language

--- .github/a3p2.py
+++ .github/a3p2.py
@@ -1,3 +1,9 @@
 from Countries import country_db
 from match import match
 from typing import List, Tuple, Callable, Any
+
+
+def get_name(country: Tuple[str, str, int, List[str]]) -> str:
+    return country[0]
+def get_language(country: Tuple[str, str, int, List[str]]) -> str:
+    return country[1]
