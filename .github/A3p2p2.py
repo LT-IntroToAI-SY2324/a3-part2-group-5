c4d341663dd82d6f5ae9a8aa917a086d739aca5f
Maged <mmothana@cps.edu>
2023-10-12 10:36:10 -0500

    another change change

--- .github/A3p2p2.py
+++ .github/A3p2p2.py
@@ -2,8 +2,14 @@ from Countries import country_db
 from match import match
 from typing import List, Tuple, Callable, Any
 
-def get_year(Countries: Tuple[str, str, int, List[str]]) -> int:
-    return Countries[2]
+def get_year(country: Tuple[str, str, int, List[str]]) -> int:
+    return country[2]
 
-def get_leaders(Countries: Tuple[str, str, int, List[str]]) -> List[str]:
-    return Countries[3]
+def get_leaders(country: Tuple[str, str, int, List[str]]) -> List[str]:
+    return country[3]
+
+def languange_by_name(matches: List[str]) -> List[str]:
+    for country in country_db:
+        if country[0] == matches:
+            return country[1]
+    return None
