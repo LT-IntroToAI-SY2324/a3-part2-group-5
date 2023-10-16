a77e5042b524eec452b5d96be3ea075c31051e86
Liam <lharvey7@cps.edu>
2023-10-16 10:37:51 -0500

    Edited pa_list

--- .github/a3p2.py
+++ .github/a3p2.py
@@ -7,6 +7,10 @@ def get_name(country: Tuple[str, str, int, List[str]]) -> str:
     return country[0]
 def get_language(country: Tuple[str, str, int, List[str]]) -> str:
     return country[1]
+def get_leaders(country: Tuple[str, str, int, List[str]]) -> List[str]:
+    return country[3]
+def get_year(country: Tuple[str, str, int, List[str]]) -> int:
+    return country[2]
 def year_by_name(matches: List[str]) -> int:
     for country in country_db:
         if country[0] == matches:
@@ -36,21 +40,50 @@ def names_before_year(year: int) -> List[str]:
         if country[2] < year:
             result.append(country[0])
     return result
+def language_by_name(matches: List[str]) -> List[str]:
+    for country in country_db:
+        if country[0] == matches:
+            return country[1]
+    return None
+
+def leaders_by_name(matches: List[str]) -> List[str]:
+    results = []
+    for country in country_db:
+        if get_name(country) == matches[0]:
+            results = get_leaders(country)
+    return results
+
+def name_by_language(matches: List[str]) -> List[str]:
+    results = []
+    for country in country_db:
+        if get_language(country) == matches[0]:
+            results.append(get_name(country))
+    return results
+def name_by_leader(matches: List[str]) -> List[str]:
+    """Finds titles of all movies that the given actor was in
+
+    Args:
+        matches - a list of 1 string, just the actor
+
+    Returns:
+        a list of movie titles that the actor acted in
+    """
+    x = []
+    for country in country_db:
+        for i in range(len(country[3])): 
+            if country[3][i] == matches[0]:
+                x.append(get_name(country))
+    return x
 pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
     (str.split("what coutries were founded in _"), names_by_year),
     (str.split("what countries were founded between _ and _"), names_between_years),
     (str.split("what countries were founded before _"), names_before_year),
     (str.split("what countries were founded after _"), names_after_year),
-    # note there are two valid patterns here two different ways to ask for the director
-    # of a movie
-    (str.split("who directed %"), director_by_title),
-    (str.split("who was the director of %"), director_by_title),
-    (str.split("what movies were directed by %"), title_by_director),
-    (str.split("who acted in %"), actors_by_title),
-    (str.split("when was % made"), year_by_title),
-    (str.split("in what movies did % appear"), title_by_actor),
-    (str.split("what movies had % and % "), title_by_two_actors),
-    (str.split("what movies had these actors: %"), title_by_actors),
+    (str.split("what is the official language of %"), language_by_name),
+    (str.split("who are the leaders of %"), leaders_by_name),
+    (str.split("what year was % founded in"), year_by_name),
+    (str.split("when was % founded"), year_by_name),
+    (str.split("what countries has % led"), name_by_leader),
     (["bye"], bye_action),
 ]
 
