70f268cd56d4f028d2b739266c8c6fbc016867cb
Liam <lharvey7@cps.edu>
2023-10-16 10:26:58 -0500

    added pa_list

--- .github/a3p2.py
+++ .github/a3p2.py
@@ -18,3 +18,39 @@ def names_by_year(year: int) -> List[str]:
         if country[2] == year:
             result.append(country[0])
     return result
+def names_between_years(years: List[int]) -> List[str]:
+    result = []
+    for country in country_db:
+        if country[2] >= years[0] and country[2] <= years[1]:
+            result.append(country[0])
+    return result
+def names_after_year(year: int) -> List[str]:
+    result = []
+    for country in country_db:
+        if country[2] > year:
+            result.append(country[0])
+    return result
+def names_before_year(year: int) -> List[str]:
+    result = []
+    for country in country_db:
+        if country[2] < year:
+            result.append(country[0])
+    return result
+pa_list: List[Tuple[List[str], Callable[[List[str]], List[Any]]]] = [
+    (str.split("what coutries were founded in _"), names_by_year),
+    (str.split("what countries were founded between _ and _"), names_between_years),
+    (str.split("what countries were founded before _"), names_before_year),
+    (str.split("what countries were founded after _"), names_after_year),
+    # note there are two valid patterns here two different ways to ask for the director
+    # of a movie
+    (str.split("who directed %"), director_by_title),
+    (str.split("who was the director of %"), director_by_title),
+    (str.split("what movies were directed by %"), title_by_director),
+    (str.split("who acted in %"), actors_by_title),
+    (str.split("when was % made"), year_by_title),
+    (str.split("in what movies did % appear"), title_by_actor),
+    (str.split("what movies had % and % "), title_by_two_actors),
+    (str.split("what movies had these actors: %"), title_by_actors),
+    (["bye"], bye_action),
+]
+
